3387816e0987863d0deafdc20a54fbcb136c8b7e
Mason Brewer <mhbrewer2@gmail.com>
2021-03-16 00:06:52 -0700

    Finished LinkedList, next is doing the binary tree.

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -14,6 +14,18 @@ namespace DataStructures {
       head = new Node(headValue);
       length = 1;
     }
+    
+    public LinkedList(int[] values) {
+      if(values.Length != 0) {
+        head = new Node(values[0]);
+        Node runner = head;
+        for(int i = 1; i < values.Length; i++) {
+          runner.next = new Node(values[i]);
+          runner = runner.next;
+        }
+      }
+      length = values.Length;
+    }
 
     public int valueAtIndex(int index) {
       if(index > length-1) {
@@ -38,6 +50,52 @@ namespace DataStructures {
         runner.next = new Node(newValue);
       }
       length++;
+    }
+
+    public void addAtIndex(int index, int newValue) {
+      if(index > length++){
+        throw new ArgumentOutOfRangeException();
+      }
+      Node runner = head;
+      Node temp = new Node(newValue);
+      if(index == 0) {
+        temp.next = head;
+        head = temp;
+      }
+      else {
+        for(int i = 1; i < index; i++) {
+          runner = runner.next;
+        }
+        temp.next = runner.next;
+        runner.next = temp;
+        length++;
+      }
+    }
+    
+    public void removeAtIndex(int index) {
+      if(index > length++){
+        throw new ArgumentOutOfRangeException();
+      }
+      Node runner = head;
+      for(int i = 1; i < index; i++) {
+        runner = runner.next;
+      }
+      runner.next = runner.next.next;
+      length++;
+    }
+    
+    override public string ToString() {
+      if(head == null) {
+        return "{}";
+      }
+      string listString = "{";
+      Node runner = head;
+      while(runner.next != null) {
+        listString += runner.value.ToString() + ",";
+        runner = runner.next;
+      }
+      listString += runner.value.ToString() + "}";
+      return listString;
     }
   }
 
--- DataStructuresTest/LinkedListTests.cs
+++ DataStructuresTest/LinkedListTests.cs
@@ -78,7 +78,7 @@ namespace DataStructuresTest
         }
         
         [TestMethod]
-        public void LinkedListEmpty_add5_ValueAt0_Returns_5()
+        public void LinkedListEmpty_Add5_ValueAt0_Returns_5()
         {
             // Arrange
             LinkedList list = new LinkedList();
@@ -87,6 +87,141 @@ namespace DataStructuresTest
             // Act
             int expected = list.valueAtIndex(0);
             int actual = 5;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedListEmpty_ToString_Returns_EmptyList()
+        {
+            // Arrange
+            LinkedList list = new LinkedList();
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList_1_2_ToString_Returns_FullList()
+        {
+            // Arrange
+            LinkedList list = new LinkedList();
+            list.add(1);
+            list.add(2);
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1,2}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList_1_2_Add3InMiddle_ToString_Returns_1_3_2()
+        {
+            // Arrange
+            LinkedList list = new LinkedList();
+            list.add(1);
+            list.add(2);
+            list.addAtIndex(1, 3);
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1,3,2}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList_AddAtIndex_1_3_ToString_Returns_1_3()
+        {
+            // Arrange
+            LinkedList list = new LinkedList();
+            list.addAtIndex(0, 1);
+            list.addAtIndex(1, 3);
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1,3}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList123_ToString_Returns_1_2_3()
+        {
+            // Arrange
+            LinkedList list = new LinkedList(new int[] {1, 2, 3});
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1,2,3}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList1_ToString_Returns_1()
+        {
+            // Arrange
+            LinkedList list = new LinkedList(new int[] {1});
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedListEmpty_ToString_Returns_Empty()
+        {
+            // Arrange
+            LinkedList list = new LinkedList(new int[] {});
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList123_RemoveAt1_ToString_Returns_1_3()
+        {
+            // Arrange
+            LinkedList list = new LinkedList(new int[] {1,2,3});
+            list.removeAtIndex(1);
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1,3}";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void LinkedList123_RemoveAt2_ToString_Returns_1_2()
+        {
+            // Arrange
+            LinkedList list = new LinkedList(new int[] {1,2,3});
+            list.removeAtIndex(2);
+
+            // Act
+            string expected = list.ToString();
+            string actual = "{1,2}";
 
             // Assert
             Assert.AreEqual(expected, actual);
